ebdcb47b473871d82b037d7e60f09def2203e9c1
crisBasti <crisporti1988@gmail.com>
2026-06-24 20:28:51 -0300

    arreglando variables de entorno en firebase

--- backend/config/firebaseAdmin.js
+++ backend/config/firebaseAdmin.js
@@ -1,16 +1,14 @@
 import { initializeApp, cert } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
-import fs from "fs";
 
-const serviceAccount = JSON.parse(
-  fs.readFileSync(
-    new URL("./serviceAccountKey.json", import.meta.url),
-    "utf8"
-  )
-);
+const serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+};
 
 const app = initializeApp({
-  credential: cert(serviceAccount)
+  credential: cert(serviceAccount),
 });
 
 export const adminAuth = getAuth(app);
