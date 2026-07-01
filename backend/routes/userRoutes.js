3bfce0046a531d1d1f7136ab27c6af214a0bac8b
crisBasti <crisporti1988@gmail.com>
2026-07-01 14:39:44 -0300

    probando ruta usuarios

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,15 +1,18 @@
 import express from "express";
 
+
 import {
   createUser,
 } from "../controllers/userController.js";
 
-const router =
-  express.Router();
+const router = express.Router();
 
-router.post(
-  "/",
-  createUser
-);
+router.get("/", (req, res) => {
+  res.json({
+    mensaje: "Ruta users funcionando"
+  });
+});
+
+router.post("/", createUser);
 
 export default router;
